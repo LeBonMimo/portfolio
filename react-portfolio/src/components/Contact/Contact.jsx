69f2583e6517cfa94739edd9c81769417d91c2d2
Guillaume SCHWAB <schwab.guillaume2310@gmail.com>
2024-10-28 15:07:33 +0100

    textarea fix + copyright

--- react-portfolio/src/components/Contact/Contact.jsx
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -9,13 +9,17 @@ export const Contact = () => {
     const linkedin = getImageUrl("contact/linkedin-logo.svg")
 
     return (
-        <section id='Contact' className="contact">
-            <div className="contact-container">  
-                
-                <ContactForm/>
-                
-            </div>
-        </section>
+        <>
+            <section id='Contact' className="contact">
+                <div className="contact-container">  
+                    
+                    <ContactForm/>
+                    
+                </div>
+                <p className='copyright'>© 2024 Guillaume Schwab - Tous droits réservés</p>
+            </section>
+        </>
+        
     );
 }
 
